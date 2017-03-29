4fc24d88c3c8a69ee9cba2cc24c26d414f3162cc
JulioJaramillo <juliopo@gmail.com>
2017-03-28 22:19:58 -0500

    Adding jest testing

--- test/App.test.js
+++ test/App.test.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import ReactTestUtils from 'react-addons-test-utils';
 import ReactDOM from 'react-dom';
 import App from '../src/components/App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+
+describe('Testing <App />', () => {
+  const renderer = ReactTestUtils.createRenderer();
+
+  beforeEach(() => {
+    renderer.render(<App />);
+  });
+
+  it('renders a react app without crashing', () => {
+    const AppElement = renderer._instance._currentElement.type;
+    expect(typeof AppElement === 'function').toBeTruthy();
+  });
+
+  it('renders more than one child', () => {
+    const result = renderer.getRenderOutput();
+    const childsAmmount = result.props.children.length;
+    expect(childsAmmount).toBeGreaterThan(1);
+  });
 });
