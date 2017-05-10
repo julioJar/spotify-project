c95ccf83376fc07924fa8de709c998bc4f4c10f5
Julio Jaramillo <julio.jaramillo@chromeriver.com>
2017-05-09 21:15:08 -0500

    Adding new scripts

---
+++ server.js
@@ -0,0 +1,18 @@
+/* eslint no-console: ["error", { allow: ["log", "error"] }] */
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const staticPath = path.join(__dirname, './build');
+const APP_PORT = process.env.APP_PORT || 3000;
+
+/* serves all the static files */
+app.use(express.static(staticPath));
+ /* serves main page */
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './build'));
+});
+
+app.listen(APP_PORT, () => {
+  console.log(`App running on localhost:${APP_PORT}`);
+});
