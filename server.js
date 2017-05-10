3cdbcedbb17c447c3e9ae868b5f85589cf4f1192
Julio Jaramillo <julio.jaramillo@chromeriver.com>
2017-05-09 21:37:15 -0500

    Adding express

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const app = express();
 const staticPath = path.join(__dirname, './build');
-const APP_PORT = process.env.APP_PORT || 3000;
+const APP_PORT = process.env.PORT || 5000;
 
 /* serves all the static files */
 app.use(express.static(staticPath));
